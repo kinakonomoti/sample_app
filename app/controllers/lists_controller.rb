84c8c63946751b7d912913bf666a78bb184c0342
AyumiShiiba <175018766+kinakonomoti@users.noreply.github.com>
2024-09-05 13:11:52 +0000

    [Finis]3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
+      @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list.id)
     else
